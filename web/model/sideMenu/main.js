33f2eb093eac91550e4a3956e633d2e591f8df20
yun <yun@yun.net.tw>
2019-10-21 17:30:16 +0800

    upload

---
+++ web/model/sideMenu/main.js
@@ -0,0 +1,31 @@
+// (function() {
+// 	function $(_selector) {
+// 		return document.querySelector ? document.querySelector(_selector) : null;
+// 	}
+
+// 	$('.sideMenu').addEventListener('click', function() {
+// 		$('.sideMenu').classList.toggle('open');
+// 		$('.sideMenu').classList.toggle('close');
+// 	});
+// })();
+
+(function($) {
+	function $$(_selector) {
+		return document.querySelector ? document.querySelector(_selector) : null;
+	}
+	function $$$(_selector) {
+		return document.querySelectorAll ? document.querySelectorAll(_selector) : [];
+	}
+	
+	var sideMenu_title = $$$('.sideMenu > .title');
+	for (var i = 0; i < sideMenu_title.length; i++) {
+		let sideMenu = sideMenu_title[i].parentNode;
+		if (!sideMenu.classList.contains('expanded')) {
+			$(sideMenu).find('ul').slideToggle(0);
+		}
+		sideMenu_title[i].addEventListener('click', function() {
+			sideMenu.classList.toggle('expanded');
+			$(sideMenu).find('ul').slideToggle(400);
+		});
+	}
+})(jQuery);
