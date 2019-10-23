90cea3622e1c3258d51c21d957e5f55a9ab740bc
yun <yun@yun.net.tw>
2019-10-23 17:48:40 +0800

    sideMenu2

--- web/model/sideMenu2/sideMenu.js
+++ web/model/sideMenu2/sideMenu.js
@@ -6,15 +6,17 @@
 		return document.querySelectorAll ? document.querySelectorAll(_selector) : [];
 	}
 	
+	let multipleExpand = true; /* 多區塊展開 */
+	
+	
 	var sideMenu_title = $$$('.sideMenu > .title');
 	for (var i = 0; i < sideMenu_title.length; i++) {
 		let sideMenu = sideMenu_title[i].parentNode;
-		if (!sideMenu.classList.contains('expanded')) {
-			$(sideMenu).find('ul').slideToggle(0);
-		}
 		sideMenu_title[i].addEventListener('click', function() {
-			sideMenu.classList.toggle('expanded');
-			$(sideMenu).find('ul').slideToggle(400);
+			if (multipleExpand) {
+				sideMenu.classList.toggle('expanded');
+				$(sideMenu).find('ul').slideToggle(400);
+			}
 		});
 	}
 })(jQuery);
